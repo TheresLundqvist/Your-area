c297940d6b2756a092fceee11bb07cb674d82d0d
TheresLundqvist <theres.lundqvist@outlook.com>
2023-05-16 18:16:48 +0000

    Add localhost to fix bug

--- your_area_store/settings.py
+++ your_area_store/settings.py
@@ -33,9 +33,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = True
 
-ALLOWED_HOSTS = ['your-area.herokuapp.com']
+ALLOWED_HOSTS = ['your-area.herokuapp.com', 'localhost']
 
 
 # Application definition
