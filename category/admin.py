633032806efeaee2d8987b6d35214e50eb64189e
TheresLundqvist <theres.lundqvist@outlook.com>
2023-05-17 18:56:49 +0000

    Add category images from admin panel

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Category
 
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('friendly_name', 'slug')
+
+
+admin.site.register(Category, CategoryAdmin)
