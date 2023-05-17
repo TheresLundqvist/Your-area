3ff5c86c3e305101f2ed33ca4f7760c47bb7f7ca
TheresLundqvist <theres.lundqvist@outlook.com>
2023-05-17 18:00:37 +0000

    Remove category from products folder and models

--- products/models.py
+++ products/models.py
@@ -1,25 +1,8 @@
 from django.db import models
 
 
-class Category(models.Model):
-    """Model for Category"""
-    class Meta:
-        """Set verbose name"""
-        verbose_name_plural = 'Categories'
-
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
-
-
 class Product(models.Model):
     """Model for Product"""
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)  # noqa
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     excerpt = models.TextField(blank=True)
