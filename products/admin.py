47d68deaf7788872e25fda9a36eba9c387dd4a26
TheresLundqvist <theres.lundqvist@outlook.com>
2023-05-22 13:28:07 +0000

    Change display for admin products

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Product
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'rating',
+        'image',
+    )
+
+
 admin.site.register(Product)
