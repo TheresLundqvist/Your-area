042c193369e6f6daede1ba8ec52f06cb347433d5
TheresLundqvist <theres.lundqvist@outlook.com>
2023-05-25 18:43:42 +0000

    Add logic to filter products by category

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product
+from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
 
@@ -11,8 +11,15 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        """ Logic to filter by category """
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        """ Logic to search for products and specific descriptions """
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +31,8 @@ def all_products(request):
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
