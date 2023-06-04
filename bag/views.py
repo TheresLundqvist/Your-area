cbd7c12a955e2c82d743ef193e3bea5ce0d7c0d7
TheresLundqvist <theres.lundqvist@outlook.com>
2023-06-04 12:27:39 +0000

    Add funtionality to remove and update links

--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse,
+    HttpResponse, get_object_or_404
+    )
+from django.contrib import messages
+from products.models import Product
+
 
 # Create your views here.
-
-
 def view_bag(request):
     """ View to return bag contents page """
 
@@ -23,3 +27,42 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 99:
+        messages.error(
+            request, 'Sorry, quantity must be less then or equal to 99.')
+    elif quantity > 0:
+        bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
