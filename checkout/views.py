3ae83a8f795a179072a7a790cb94ad9351521bdc
TheresLundqvist <theres.lundqvist@outlook.com>
2023-06-04 18:53:40 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NFLIvD76A0f0OXIjlU58D9Mb01jpxZDgZ6IIFsFCwSeCv8lo6I408op4li7EtCz5Q3IeIbKj0oeO4ulY7s5kLOB00iwAEowpJ',  # noqa
+        'client_secret': 'sk_test_51NFLIvD76A0f0OXIfNNJs7Dd1vEeTZ2Qqnzle2GKgzCUMn1AVwog3AMN1n3KoYhtGJwKZIssa6YLOQNBeGHhVVAI008sbNOHws',  # noqa
     }
 
     return render(request, template, context)
