15500ccf56896bd5297eee281f05e1172c603b5e
TheresLundqvist <theres.lundqvist@outlook.com>
2023-06-07 16:24:48 +0000

    Style add product template

--- products/forms.py
+++ products/forms.py
@@ -15,4 +15,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black'
+            field.widget.attrs['class'] = 'my-2 border-black'
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -69,3 +70,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
