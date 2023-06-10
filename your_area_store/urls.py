b33130cbb5641467c6dc1f5ae05ecc976cadb0eb
TheresLundqvist <theres.lundqvist@outlook.com>
2023-06-10 16:01:08 +0000

    Add 404 page

--- your_area_store/urls.py
+++ your_area_store/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +33,4 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('gallery/', include('gallery.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'your_area_store.views.handler404'
