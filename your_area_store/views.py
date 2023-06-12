ddfe20df3beb033cf8b11cd30abe61c70be79c6f
TheresLundqvist <theres.lundqvist@outlook.com>
2023-06-12 08:50:59 +0000

    Add error handler 500 - internal server error

--- your_area_store/views.py
+++ your_area_store/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
 
+def handler400(request, exception):
+    """ Error Handler 400 - Bad Request """
+    return render(request, "errors/400.html", status=400)
+
+
+def handler403(request, exception):
+    """ Error Handler 403 - Page Forbidden """
+    return render(request, "errors/403.html", status=403)
+
+
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
 
 
-def handler403(request, exception):
-    """ Error Handler 403 - Page Not Found """
-    return render(request, "errors/403.html", status=403)
-
-
-def handler400(request, exception):
-    """ Error Handler 400 - Page Not Found """
-    return render(request, "errors/400.html", status=400)
+def handler500(request):
+    """ Error Handler 500 - Internal Server Error """
+    return render(request, "errors/500.html", status=500)
